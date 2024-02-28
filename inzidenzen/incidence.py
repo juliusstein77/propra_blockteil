a44fc7850dc1054820ecdb95d932fe367f2bd594
Malte Weyrich <malte.weyrich@hotmail.de>
2024-02-28 15:44:44 +0100

    raw code for calculations

--- inzidenzen/incidence.py
+++ inzidenzen/incidence.py
@@ -18,15 +18,52 @@ def main(path_to_fasta: str, sequences: list):
     # print(genome_seq)
     # print(set(genome_seq))
     # print(len(set(genome_seq)))
+    print("Tatsächliches vorkommen der seqs")
     for seq in sequences:
         pattern = rf'(?=({seq}))'
         matches = re.finditer(pattern, genome_seq)
         m_count = 0
-
-        for m in matches:
+        for _ in matches:
             m_count+=1
             
         print(f'{seq}: {m_count}')
+    ###################################################
+    # CALCULATIONS                                    #
+    ###################################################
+    base_content = {}
+    for base in ['A', 'C', 'G', 'T']:
+        m_count=0
+        pattern = rf'{base}'
+        matches = re.finditer(pattern, genome_seq)
+        for _ in matches:
+            m_count+=1
+            
+        print("Relative Häufigkeit der basen")
+        # base_content[base] = f'{m_count/len(genome_seq)*100, 2}%'
+        print(f'{base}: {m_count/len(genome_seq)*100}')
+        base_content[base] = m_count/len(genome_seq)
+
+    relative_prob = {} 
+    print("1/4**len(seq)*len(genome)")
+    for seq in sequences:
+        relative_prob[seq] = 0.25**len(seq)*len(genome_seq)
+        print(seq, 0.25**len(seq)*len(genome_seq))
+    
+    print("ac_prob")
+    for seq in sequences:
+        prob = 1
+        for base in seq:
+            prob*=base_content[base]
+        print(prob*len(genome_seq))
+
+
+            
+            
+   
+
+        
+
+
 
 
 
