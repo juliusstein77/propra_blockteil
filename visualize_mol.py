a8f9373b8ecc0345c06a6ba209a6a52c00094df1
Malte Weyrich <malte.weyrich@hotmail.de>
2024-02-29 20:46:34 +0100

    formatting

--- visualize_mol.py
+++ visualize_mol.py
@@ -1,41 +1,39 @@
 #!/usr/bin/python3
 
 import argparse
-import requests
 import os
 import subprocess
+import get_pdb
 
 DATADIR="data/"
 SCRIPTPATH="jmol_script.spt"
 
 def retrive_pdb_id(pdb_id):
-    """Retrives pdb_id as file into an out.pdb.
+    """
+    Retrives pdb_id as file into an out.pdb.
 
     :pdb_id: id of pdb to look up in db
     :returns: the path to out.pdb
-
     """
 
     # check if DATADIR exists 
     if not os.path.exists(DATADIR):
             os.makedirs(DATADIR)
 
-    #  FIX: implement Julius version of this code
-    url = f"https://files.rcsb.org/download/{pdb_id}.pdb"
-    out_pdb_path = f"{DATADIR}{pdb_id}.pdb"
-    response = requests.get(url)
+    content = get_pdb.download_pdb(pdb_id, False)
+    if content == None:
+        print(f"Received no content for {pdb_id}")
 
-    if response.status_code == 200:
-        with open(out_pdb_path, 'wb') as f:
-            f.write(response.content)
-    else:
-        print(f"Failed to retrieve PDB file for ID {pdb_id}.\nStatus code: {response.status_code}")
+    # save to data dir
+    out_pdb_path = f"{DATADIR}{pdb_id}.pdb"
+    get_pdb.save_to_file(content, out_pdb_path)
 
     return out_pdb_path
 
 
 def make_script(path_to_pdb: str, color: bool, png_out: str):
-    """writes the jmol script into stript.txt.
+    """
+    writes the jmol script into stript.txt.
 
     :path_to_pdb: 
     :color: a bool which if True adds an additional line to the script
@@ -58,7 +56,8 @@ def make_script(path_to_pdb: str, color: bool, png_out: str):
 
 
 def run_jmol(script_file):
-    """run jmol with the specified script file.
+    """
+    run jmol with the specified script file.
 
     :script_file: path to the created script 
     """
@@ -68,11 +67,12 @@ def run_jmol(script_file):
 
 
 def main(id: str, output: str, is_html: bool, is_color: bool):
-    """Manages all inputs of program.
+    """
+    manages all inputs of program.
 
     :id: pdb id to download
     :output: dir for .pngs
-    :is_html: flag to switch to html output
+    :is_html: flag to switch to jsmol # TODO
     :is_color: colorize secondary structure
     """
     
@@ -83,11 +83,11 @@ def main(id: str, output: str, is_html: bool, is_color: bool):
 
 if __name__ == "__main__":
     # init parser
-    parser = argparse.ArgumentParser(description="description")
+    parser = argparse.ArgumentParser(description="Visualize a pdb file.")
     parser.add_argument('--id', type=str, required=True)
-    parser.add_argument('--output', type=str, required=False, default="")
-    parser.add_argument('--html', type=str, required=False)
-    parser.add_argument('--colourized', action='store_true', required=False)
+    parser.add_argument('--output', type=str, required=False, default="", help="output dir for .pngs")
+    parser.add_argument('--html', type=str, required=False, default="", help="activate jsmol")
+    parser.add_argument('--colourized', action='store_true', required=False, default=False, help="colorize secondary structure")
 
     args = parser.parse_args()
 
