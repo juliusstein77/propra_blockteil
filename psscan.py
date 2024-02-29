b0e629632d687c19ced66e61ac4c25e80210052b
Julius Stein <120008299+juliusstein77@users.noreply.github.com>
2024-02-29 12:01:21 +0100

    added comments

--- psscan.py
+++ psscan.py
@@ -14,7 +14,8 @@ def prosite_to_regex(prosite_pattern):
     regex_pattern = prosite_pattern.replace('[', '[').replace(']',']').replace('{', '[^').replace('}', ']').replace('(', '{').replace(')', '}').replace('<', '').replace('>', '')
     # Replace individual characters with regular expression equivalents
     regex_pattern = regex_pattern.replace('x', '.').replace('-', '').replace('*', '.*')
-    # Add lookahead and lookbehind assertions to the pattern -> necessary for overlapping matches
+    # . matches any single character, * quantifier matches zero or more occurrences of any character
+    # positive lookahead assertion at the beginning of the pattern to find overlapping matches
     regex_pattern = '(?=(' + regex_pattern + '))'
     return regex_pattern
 
@@ -26,8 +27,10 @@ def find_pattern_matches(pattern, sequence, sequence_id):
     matches = re.finditer(regex_pattern, sequence)
     results = []
     for match in matches:
+        # retrieve first capturing group from the match
         matched_sequence = match.group(1)
         start_position = match.start() + 1
+        # add tupel to results list
         results.append((sequence_id, start_position, matched_sequence))
     return results
 
@@ -40,6 +43,7 @@ def load_sequence_from_fasta(fasta_file):
     for line in source:
         line = line.strip()
         if line.startswith('>'):
+            # remove leading '>' and leading/trailing whitespace
             current_id = line.strip()[1:]
             sequences[current_id] = ''
         else:
@@ -60,6 +64,11 @@ def load_prosite_pattern(prosite_id):
         # Check if the request was successful
         if response.status_code == 200:
             # Extract the Prosite pattern from the text file
+            # \s* zero or more whitespace characters
+            # \[.*?\] match the first occurrence of a square bracket and everything inside it
+            # .*? match any character (except for line terminators) zero or more times
+            # ? non-greedy match, lazy match: match as few characters as possible
+            # (?=\n-) positive lookahead assertion: match the pattern only if it is followed by a newline and a hyphen
             pattern_match = re.search(r'-Consensus pattern:\s*(\[.*?\].*?)(?=\n-)', response.text, re.DOTALL)
             if pattern_match:
                 prosite_pattern = pattern_match.group(1)
@@ -71,6 +80,8 @@ def load_prosite_pattern(prosite_id):
                 for line in response.text.split('\n'):
                     if line.startswith('PA'):  # Check if the line starts with "PA" followed by a tab
                         # Extract the pattern
+                        # split the line at the first space and take the second part
+                        # remove leading and trailing whitespace and trailing dot
                         prosite_pattern = line.split(' ', 1)[1].strip().rstrip('.')
                         #print(prosite_pattern)
                         return prosite_pattern
