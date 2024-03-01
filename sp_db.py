59f1c5cdb3ec20ed3a4614aba1ce989b8d1d3aa3
Jan Philipp Hummel <hummelj@jaspis.cip.ifi.lmu.de>
2024-03-01 16:15:31 +0100

    bugfix

--- sp_db.py
+++ sp_db.py
@@ -10,22 +10,91 @@ from collections import defaultdict
 import pprint
 
 
-cnx = mysql.connector.connect(user='bioprakt3', password='$1$dXmWsf6J$rQWMUrRzyAhhqjPscdRbG.',
-                              host='mysql2-ext.bio.ifi.lmu.de',
-                              database='bioprakt3',
-                              port='3306')
-cursor = cnx.cursor()
-parser = argparse.ArgumentParser()
-parser.add_argument('--input', type=str, help='Input Swissprot file', required=True)
-args = parser.parse_args()
+def insert_keywords(sequence_id, type, keyword):
+    cursor.execute("INSERT INTO Keywords (seq_id, type, value) VALUES (%s, %s, %s)", (sequence_id, type, keyword))
 
-swissprots = []
-file = args.input
+def insert_swissprot(swissprot):
+    seq = swissprot['seq']['strct']
+    source_id = swissprot['id']
+    source = 'swissprot'
+    type = 'protein'
 
-def get_swissprot(filename):
-    # set to store the results
-    results = set()
+    cursor.execute("SELECT id FROM Sequences WHERE source_id = %s", (source_id,))
+    sequence_row = cursor.fetchone()
+
     
+    existing_keywords = {}
+
+    if not sequence_row:
+        cursor.execute("INSERT INTO Sequences (type, sequence, source, source_id) VALUES (%s, %s, %s, %s)", (type, seq, source, source_id))
+        cursor.execute("SELECT LAST_INSERT_ID()")
+        sequence_id = cursor.fetchone()[0]
+
+    else:
+        sequence_id = sequence_row[0]
+        cursor.execute("SELECT type, value FROM Keywords WHERE seq_id = %s", (sequence_id,)) 
+        existing_keywords = []
+        for keyword in cursor.fetchall():
+            existing_keywords.append({keyword[0]: keyword[1]})
+    
+    if 'ac' in swissprot:
+        for ac in swissprot['ac']:
+            if {'ac': ac} not in existing_keywords:
+                if debug:
+                    print(f'inserting ac for {sequence_id} with value {ac}')
+                insert_keywords(sequence_id, 'ac', ac)
+
+    if 'de' in swissprot:
+        if {'de': swissprot['de']} not in existing_keywords:	
+            if debug:
+                print(f'inserting de for {sequence_id} with value {swissprot["de"]}')    
+            insert_keywords(sequence_id, 'de', swissprot['de'])
+
+    if 'ref' in swissprot:
+        for ref in swissprot['ref']:
+            if {'rn': ref['rn']} not in existing_keywords:
+                if debug:
+                    print(f'inserting rn for {sequence_id} with value {ref["rn"]}')
+                insert_keywords(sequence_id, 'rn', ref['rn'])
+            if 'ra' in ref and {'ra': ref['ra']} not in existing_keywords:
+                if debug:
+                    print(f'inserting ra for {sequence_id} with value {ref["ra"]}')
+                insert_keywords(sequence_id, 'ra', ref['ra'])
+            if 'rt' in ref and {'rt': ref['rt']} not in existing_keywords:
+                if debug:
+                    print(f'inserting rt for {sequence_id} with value {ref["rt"]}')
+                insert_keywords(sequence_id, 'rt', ref['rt'])
+            if 'rl' in ref and {'rl': ref['rl']} not in existing_keywords:
+                if debug:
+                    print(f'inserting rl for {sequence_id} with value {ref["rl"]}')
+                insert_keywords(sequence_id, 'rl', ref['rl'])
+            
+    
+    if 'dr' in swissprot:
+        for dr in swissprot['dr']:
+            if {'dr': dr} not in existing_keywords:
+                if debug:
+                    print(f'inserting dr for {sequence_id} with value {dr}')
+                insert_keywords(sequence_id, 'dr', dr)
+
+    if 'ft' in swissprot:
+        for ft in swissprot['ft']:
+            if {'ft': ft} not in existing_keywords:
+                if debug:
+                    print(f'inserting ft for {sequence_id} with value {ft}')
+                insert_keywords(sequence_id, 'ft', ft)
+
+    
+    if {'sq': swissprot['seq']['sq']} not in existing_keywords:
+        if debug:
+            print(f'inserting sq for {sequence_id} with value {swissprot["seq"]["sq"]}')
+        insert_keywords(sequence_id, 'sq', swissprot['seq']['sq'])
+
+
+                    
+                
+
+def get_swissprot(filename):    
     with open(filename, 'r') as file:    
         swissprot_protein = {}
         seq_state = False
@@ -33,6 +102,7 @@ def get_swissprot(filename):
             if seq_state:
                 if line.startswith(' '):
                     sq = line.split(" ", 1)[1].strip()
+                    sq = re.sub(r'\s+', '', sq)
                     if 'strct' in swissprot_protein['seq']:
                         swissprot_protein['seq']['strct'] += sq
                     else:
@@ -43,6 +113,7 @@ def get_swissprot(filename):
             elif line.startswith('ID'): 
                 if swissprot_protein: # reinitialize dictionary for new protein entry
                     #print(swissprot_protein)
+                    insert_swissprot(swissprot_protein)
                     swissprots.append(swissprot_protein)
                     swissprot_protein = {}
                 id = line.split("ID", 1)[1].strip().split(" ")[0].strip()
@@ -109,59 +180,23 @@ def get_swissprot(filename):
             elif line.startswith('SQ'):
                 sq = line.split("SQ", 1)[1].strip()
                 swissprot_protein['seq'] = {'sq': sq}
-                seq_state = True
+                seq_state = True  
     
 
-           
-                    
-            
-
-
-            
-            
-                
-
-                
-
-               
-                
-
-                    
-                    
-            
-                
-                
-
-                
-        
-     
-
-            
-            
-            
-    """
-            # If inside an entry and the line starts with 'AC', store the AC numbers
-            elif line.startswith('AC'):
-                ac_line = line.split("AC", 1)[1].strip()  # Split the line and remove leading/trailing whitespace
-                ac_ids = ac_line.split(";")  # Split the accession numbers separated by ';'
-                # Remove leading/trailing whitespace from each accession number and add it to the entry AC set
-                ac_ids = [ac.strip() for ac in ac_ids if ac.strip()]
-                entry_ac.update(ac_ids)
-            # If inside an entry and the line starts with 'KW', check for keywords
-            elif entry_ac and line.startswith('KW'):
-                kw_temp = line.split("KW", 1)[1].strip()  # Split the line and remove leading/trailing whitespace
-                kw_temp = kw_temp.split(";")  # Split the keywords separated by ';'
-                keywords_list = [kw.strip().rstrip('.') for kw in kw_temp]
-                for keyword in keywords:
-                    # If the whole keyword is found in the line, add the entry AC to the results set
-                    if keyword in keywords_list:
-                        results.update(entry_ac)"""
-
-    # Sort the results and return them
-    #return sorted(results)
-
+cnx = mysql.connector.connect(user='bioprakt3', password='$1$dXmWsf6J$rQWMUrRzyAhhqjPscdRbG.',
+                              host='mysql2-ext.bio.ifi.lmu.de',
+                              database='bioprakt3',
+                              port='3306')
+cursor = cnx.cursor()
+parser = argparse.ArgumentParser()
+parser.add_argument('--input', type=str, help='Input Swissprot file', required=True)
+parser.add_argument('--debug', action='store_true', help='Print debug information', required=False)
+args = parser.parse_args()
+debug = args.debug
+swissprots = []
+file = args.input           
 get_swissprot(file)
-test = swissprots[:2]
+test = swissprots[:1]
 print(test)
 cursor.close()
 cnx.close()
