fe964e476185935ffd730018fc7fb4928e5f4cc0
Jan Philipp Hummel <hummelj@jaspis.cip.ifi.lmu.de>
2024-03-02 15:08:42 +0100

    bugfix

--- get_seq.py
+++ get_seq.py
@@ -2,6 +2,13 @@
 import mysql.connector
 from mysql.connector import errorcode
 import argparse
+def to_fasta(seq):
+    return ">"+str(list(seq.keys())[0])+"\n"+str(list(seq.values())[0])
+
+
+def get_sequence(source_id, source):
+    cursor.execute("SELECT `sequence` FROM Sequences WHERE `source_id` = %s AND `source` = %s", (source_id, source))
+    return {source_id:cursor.fetchone()[0]}
 
 
 cnx = mysql.connector.connect(user='bioprakt3', password='$1$dXmWsf6J$rQWMUrRzyAhhqjPscdRbG.',
@@ -11,30 +18,15 @@ cnx = mysql.connector.connect(user='bioprakt3', password='$1$dXmWsf6J$rQWMUrRzyA
 cursor = cnx.cursor()
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--id', type=str, help='Id of Sequenc')
+parser.add_argument('--id', type=str, help='Id of Sequence', nargs='+')
 parser.add_argument('--source', type=str, help='Input Source of Sequence')
 args = parser.parse_args()
 
-select_string = "SELECT `id`, `sequence` FROM Sequences " \
-                "WHERE `id` in ("
-for id in args.id:
-    select_string += id
-    select_string += ","
 
-select_string = select_string[0:len(select_string)-1]
-select_string += ")"
-
-try:
-    # write in fasta file
-    cursor.execute(select_string)
-    with cnx.cursor() as cursor:
-        cursor.execute(select_string)
-        rows = cursor.fetchall()
-        
-finally:
-    cursor.close()
-    cnx.close()
-for row in rows:
-            print(">"+row[0])
-            print(row[1])
-
+if args.id and args.source:
+    seqs = []
+    for id in args.id:
+        seqs.append(get_sequence(id, args.source))
+    for seq in seqs:
+        print(to_fasta(seq))
+    
