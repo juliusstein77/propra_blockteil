205f7bef26b02a5f4acfb99015e2b15a9f0c7c2a
maxi <hansenm@cip.ifi.lmu.de>
2024-03-02 16:55:32 +0100

    Fixed File-Handling, fixed Regex
     added MD-Tag-Example in German

--- filter_crispr.py
+++ filter_crispr.py
@@ -12,30 +12,44 @@ SAM_COLS = {"HEADER": 0,
 
 def parse_sam_file(sam):
     sam_data = []
-    with open(sam, 'r') as f:
-        for line in f:
-            # Skip SAM Headers
-            if not line.startswith('@'):
-                columns = line.strip().split('\t')
-                header = columns[SAM_COLS['HEADER']]
-                cigar = columns[SAM_COLS['CIGAR']]
-                seq = columns[SAM_COLS['SEQ']]
-                if len(columns) > SAM_COLS['MD']:
-                    md = columns[SAM_COLS['MD']]
-                else:
-                    md = None
-                sam_data.append((header, cigar, seq, md))
+    for line in sam:
+        # Skip SAM Headers
+        if not line.startswith('@'):
+            columns = line.strip().split('\t')
+            header = columns[SAM_COLS['HEADER']]
+            cigar = columns[SAM_COLS['CIGAR']]
+            seq = columns[SAM_COLS['SEQ']]
+            if len(columns) > SAM_COLS['MD']:
+                md = columns[SAM_COLS['MD']]
+            else:
+                md = None
+            sam_data.append((header, cigar, seq, md))
     return sam_data
+
+
+""" MD-Tag Beispiel:
+Z:4T5G16A23 bedeutet:
+4 richtige Nukleotide in Folge
+dann ein T
+5 richtige in Folge
+dann ein G
+dann 16 richtige
+dann ein A
+dann 23 richtige
+--> sprich man will, dass man zum Schluss 0 oder einen richtigen hat, sowieso die vorletzte Position keine Zahl ist 
+(keine 10 / 11 / 20 / 21 / 30 /...)
+"""
 
 
 def filter_with_mismatch(sam_data, with_mismatch):
     for data in sam_data:
         if data[3] is None:
             continue
-        if re.search(r'[^8-9][01]$', data[3]):
+        if re.search(r'^MD.*[^0-9][01]$', data[3]):
             with_mismatch.write(f'>{data[0]}\n{data[2]}\n')
 
 
+# Nach Server schon mal korrekt:
 def filter_no_off_targets(sam_data, no_off_targets):
     for data in sam_data:
         if data[1] == '*':
@@ -45,13 +59,11 @@ def filter_no_off_targets(sam_data, no_off_targets):
 def main():
     parser = argparse.ArgumentParser(description='Filter aligned sequences based on criteria.')
     parser.add_argument('--sam', help='Input SAM file', required=True, type=argparse.FileType('r'))
-    parser.add_argument('--no-off-targets', help='Output file for sequences with no off-targets', required=True, type=argparse.FileType('w'))
-    parser.add_argument('--with-mismatch', help='Output file for sequences with mismatches in GG suffix', required=True,type=argparse.FileType('w'))
+    parser.add_argument('--no-off-targets', help='Output file for sequences with no off-targets', required=True,
+                        type=argparse.FileType('w'))
+    parser.add_argument('--with-mismatch', help='Output file for sequences with mismatches in GG suffix', required=True,
+                        type=argparse.FileType('w'))
     args = parser.parse_args()
-
-    #sam_input = args.sam
-    #if sam_input == '-':
-    #    sam_input = sys.stdin
 
     sam_data = parse_sam_file(args.sam)
 
