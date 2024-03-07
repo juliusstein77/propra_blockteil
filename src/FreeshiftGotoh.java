ac8a0828f42a32f5f5a528648df4f9c82611ebd9
github4touchdouble <touchedouble@gmx.de>
2024-03-07 18:55:39 +0100

    added gotoh

---
+++ src/FreeshiftGotoh.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class FreeshiftGotoh extends Gotoh {
+
+    public void initialize() {
+        for (int i = 1; i < s.length() + 1; i++) {
+            mx_d[i][0] = Double.NEGATIVE_INFINITY;
+        }
+        //Initializing state mx_i
+        for (int j = 1; j < t.length() + 1; j++) {
+            mx_i[0][j] = Double.NEGATIVE_INFINITY;
+        }
+        for (int i = 1; i < s.length() + 1; i++) {
+            for (int j = 1; j < t.length() + 1; j++) {
+                mx_i[i][j] = Math.max(mx_a[i - 1][j] + gap_open_penalty + gap_extend_penalty, mx_i[i - 1][j] + gap_extend_penalty);
+                mx_d[i][j] = Math.max(mx_a[i][j - 1] + gap_open_penalty + gap_extend_penalty, mx_d[i][j - 1] + gap_extend_penalty);
+                mx_a[i][j] = Math.max(mx_a[i - 1][j - 1] + get_score(s.charAt(i - 1), t.charAt(j - 1)), Math.max(mx_d[i][j], mx_i[i][j]));
+            }
+        }
+        int[] max = get_max_last(mx_a);
+        this.score = mx_a[max[0]][max[1]];
+    }
+
+    public void backtrack() {
+        ArrayList<int[]> local_bts = new ArrayList<int[]>();
+        int[] max = get_max_last(mx_a);
+        int i = max[0];
+        int j = max[1];
+        // -- start from the maximum value in the matrix and go to the border of the matrix
+        while (i > 0 && j > 0) {
+            // --- diagonal --- >>> match/mismatch
+            if (mx_a[i][j] == (get_score(s.charAt(i - 1), t.charAt(j - 1)) + mx_a[i - 1][j - 1])) {
+                local_bts.add(new int[]{i, j});
+                --i;
+                --j;
+            }
+            // --- up --- >>> gap in t
+            else if (mx_a[i][j] == mx_i[i][j]) {
+                local_bts.add(new int[]{i, j});
+                int counter = 1;
+                while (counter < i) {
+                    if ((mx_a[i - counter][j] + gap_open_penalty + gap_extend_penalty * counter) == mx_a[i][j]) {
+                        break;
+                    }
+                    ++counter;
+                }
+                i -= counter; // continue after the gap
+            }
+            // -- left --- >>> gap in s
+            else if (mx_a[i][j] == mx_d[i][j]) {
+                local_bts.add(new int[]{i, j});
+                int counter = 1;
+                while (counter < j) {
+                    if ((mx_a[i][j - counter] + gap_open_penalty + gap_extend_penalty * counter) == mx_a[i][j]) {
+                        break;
+                    }
+                    ++counter;
+                }
+                j -= counter; // continue after the gap
+            }
+        }
+        // >>> add the first position
+        local_bts.add(new int[]{i, j});
+        this.bts = local_bts;
+    }
+
+
+    public FreeshiftGotoh(String s, String t, HashMap<String, Double> scoring_matrix, int gap_open_penalty, int gap_extend_penalty) {
+        super(s, t, scoring_matrix, gap_open_penalty, gap_extend_penalty);
+        initialize();
+        backtrack();
+        calculate_alignment();
+        this.score = mx_a[bts.get(0)[0]][bts.get(0)[1]];
+        // >>> pull the sole alignment into the whole sequence
+        this.alignment = calculate_final_alignment(bts.get(0)[0], bts.get(bts.size() - 1)[0], bts.get(0)[1], bts.get(bts.size() - 1)[1]);
+    }
+}
---
+++ src/Gotoh.java
@@ -0,0 +1,169 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Gotoh {
+    public double score;
+    public final String s;
+    public final String t;
+    public final int gap_open_penalty;
+    public final int gap_extend_penalty;
+    public final HashMap<String, Double> scoring_matrix;
+    public double[][] mx_a;
+    public double[][] mx_d;
+    public double[][] mx_i;
+    public ArrayList<int[]> bts;
+    public String alignment;
+
+    public Gotoh(String s, String t, HashMap<String, Double> scoring_matrix, int gap_open_penalty, int gap_extend_penalty) {
+        this.s = s;
+        this.t = t;
+        this.scoring_matrix = scoring_matrix;
+        this.gap_open_penalty = gap_open_penalty;
+        this.gap_extend_penalty = gap_extend_penalty;
+        this.mx_a = new double[s.length() + 1][t.length() + 1];
+        this.mx_d = new double[s.length() + 1][t.length() + 1];
+        this.mx_i = new double[s.length() + 1][t.length() + 1];
+        this.bts = new ArrayList<int[]>();
+        this.alignment = "";
+    }
+
+    public void calculate_alignment() {
+        StringBuilder a = new StringBuilder();
+        StringBuilder b = new StringBuilder();
+        for (int l = 0; l < bts.size() - 1; l++) {
+            int i = bts.get(l)[0];
+            int j = bts.get(l)[1];
+            int x = bts.get(l + 1)[0];
+            int y = bts.get(l + 1)[1];
+            // --- diagonal --- >>> match/mismatch
+            if (not_gap(i, j, x, y)) {
+                a.insert(0, s.charAt(i - 1));
+                b.insert(0, t.charAt(j - 1));
+            }
+            // --- left --- >>> gap in s
+            else if (gap_in_s(i, j, x, y)) {
+                HashMap<StringBuilder, StringBuilder> gap = add_gap(i, j, x, y, a, b, s, "-");
+                a = gap.keySet().iterator().next();
+                b = gap.values().iterator().next();
+            }
+            // --- up --- >>> gap in t
+            else if (gap_in_t(i, j, x, y)) {
+                HashMap<StringBuilder, StringBuilder> gap = add_gap(j, i, y, x, b, a, t, "-");
+                b = gap.keySet().iterator().next();
+                a = gap.values().iterator().next();
+            }
+
+        }
+        this.alignment = a.toString() + "\n" + b.toString();
+    }
+
+    private boolean not_gap(int i, int j, int x, int y) {
+        return i == x + 1 && j == y + 1;
+    }
+
+    private boolean gap_in_t(int i, int j, int x, int y) {
+        return i == x && j > y;
+    }
+
+    private boolean gap_in_s(int i, int j, int x, int y) {
+        return i > x && j == y;
+    }
+
+    private HashMap<StringBuilder, StringBuilder> add_gap(int q, int w, int e, int r, StringBuilder a, StringBuilder b, String seq, String placeholder) {
+        int gap = q - e;
+        int count = 1;
+        while (gap > 0) {
+            a.insert(0, seq.charAt(q - count));
+            b.insert(0, placeholder);
+            --gap;
+            ++count;
+        }
+        return new HashMap<StringBuilder, StringBuilder>() {{
+            put(a, b);
+        }};
+    }
+
+    public String calculate_final_alignment(int index_A, int index_a, int index_B, int index_b) {
+        StringBuilder a = new StringBuilder();
+        StringBuilder b = new StringBuilder();
+
+        // --- if the alignment is not empty, return the sequences with the alignment ---
+        if (!alignment.equals("\n")) {
+            String[] aligned_sequences = alignment.split("\n");
+            a.append(aligned_sequences[0]);
+            b.append(aligned_sequences[1]);
+            a.insert(0, s.substring(0, index_a));
+            for (int i = 0; i < index_a; i++) {
+                b.insert(0, "-");
+            }
+            b.insert(0, t.substring(0, index_b));
+            for (int j = 0; j < index_b; j++) {
+                a.insert(0, "-");
+            }
+            a.append(s.substring(index_A, s.length()));
+            b.append("-".repeat(Math.max(0, s.length() - index_A)));
+            b.append(t.substring(index_B, t.length()));
+            a.append("-".repeat(Math.max(0, t.length() - index_B)));
+        }
+        // --- if the alignment is empty, return the sequences followed by gaps placeholders with the length of the sequences ---
+        else {
+            a.append(s);
+            a.append("-".repeat(s.length()));
+            b.append(t);
+            b.append("-".repeat(t.length()));
+        }
+        return a.toString() + "\n" + b.toString();
+    }
+
+    public double get_score(char a, char b) {
+        String ab = Character.toString(a) + Character.toString(b);
+        if (scoring_matrix.containsKey(ab)) {
+            return scoring_matrix.get(ab);
+        } else return scoring_matrix.get(Character.toString(b) + Character.toString(a));
+    }
+
+    public int[] get_max(double[][] mx) {
+        double bin = Double.NEGATIVE_INFINITY;
+        int r_max = -1;
+        int c_max = -1;
+
+        for (int r = 0; r < mx.length; r++) {
+            for (int c = 0; c < mx[r].length; c++) {
+                if (mx[r][c] > bin) {
+                    bin = mx[r][c];
+                    r_max = r;
+                    c_max = c;
+                }
+            }
+        }
+
+        return new int[]{r_max, c_max};
+    }
+
+    public int[] get_max_last(double[][] mx) {
+        double r_bin = Double.NEGATIVE_INFINITY;
+        double c_bin = Double.NEGATIVE_INFINITY;
+        int[] index_r = new int[2];
+        int[] index_c = new int[2];
+        for (int i = 0; i < mx[0].length; i++) {
+            if (mx[mx.length - 1][i] >= r_bin) {
+                r_bin = mx[mx.length - 1][i];
+                index_r[0] = mx.length - 1;
+                index_r[1] = i;
+            }
+        }
+        for (int j = 0; j < mx.length; j++) {
+            if (mx[j][mx[0].length - 1] >= c_bin) {
+                c_bin = mx[j][mx[0].length - 1];
+                index_c[0] = j;
+                index_c[1] = mx[0].length - 1;
+            }
+        }
+
+        if (mx[index_r[0]][index_r[1]] > mx[index_c[0]][index_c[1]]) {
+            return index_r;
+        } else return index_c;
+    }
+
+
+}
